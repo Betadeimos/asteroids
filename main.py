04d4a5d1f0892a35d6c344e2a3a1c34329fd3b43
Betadeimos <davide.chincoli@gmail.com>
2025-05-22 23:54:08 +0100

    added black window and ignore

---
+++ main.py
@@ -0,0 +1,26 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+import constants
+
+def main():
+    pygame.init()
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
