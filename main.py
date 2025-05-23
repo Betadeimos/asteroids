31377e2a9e19b79f0e313e0173cb14348bf9b28d
Betadeimos <davide.chincoli@gmail.com>
2025-05-23 23:50:08 +0100

    added frame rate

--- main.py
+++ main.py
@@ -2,20 +2,28 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from pygame.time import Clock
 from constants import *
 import constants
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
+        # make the close button work
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # make a black screen
         screen.fill("black")
         pygame.display.flip()
+
+        # limit framerate to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
